10752db409a42b66aba8213718b87751ce7e7777
Yosin Chakalov <esin94234@gmail.com>
2025-07-08 14:56:21 +0500

    first commit

---
+++ classtask.py
@@ -0,0 +1,86 @@
+# n = int(input())
+# sum = 0
+# a = 1
+# while a<=n:
+#     sum+=a
+#     a+=1
+# print(sum)
+
+# n = int(input())
+# prod = 1
+# a = 1
+# while a<=n:
+#     prod*=a
+#     a+=1
+# print(prod)
+
+# n = int(input())
+# a = 1
+# while a<=n:
+#     if n%a==0:
+#         print(a)
+#     a+=1
+
+# n = int(input())
+# sum = 0
+# a = 1
+# while a<=n:
+#     if n%a==0:
+#         sum+=1
+#     a+=1
+# if sum==2:
+#     print("prime")
+# else:
+#     print("not prime")
+    
+# n = int(input())
+# cnt = 0
+# i = 1
+# while i<=n:
+#     if i==5:
+#         continue
+#     print(i)
+#     i+=1
+
+# n = int(input())
+# reverse = 0
+# while n>0:
+#     m=n%10
+#     reverse=reverse*10+m
+#     n=n//10
+# print(reverse)
+
+# cnt = 0
+# while True:
+#     a = int(input())
+#     if a==0:
+#         break
+#     cnt+=a
+# print(cnt)
+
+# n = int(input())
+# for i in range(1,n+1):
+#     if n%i==0:
+#         print(i)
+
+# n = int(input("enter your num --> "))
+# cnt=0
+# cnt2=0
+# for i in range(1,n+1):
+#     cnt+=i**2
+#     cnt2+=i*i
+# print(cnt,cnt2)
+
+# n = int(input("enter your number --> "))
+# cnt = 2
+# for i in range(1,n):
+#     cnt=cnt*2
+# print(cnt)
+
+# n = int(input("enter your number --> "))
+# p = int(input(f"enter the pover of {n} --> "))
+# cnt=0
+# for i in range(p+1):
+#     cnt+=n**i
+# print(cnt)
+
---
+++ hometask2.py
@@ -0,0 +1,155 @@
+# --- for
+
+#1
+# a = int(input("enter your num --> "))
+# b = int(input("enter your num --> "))
+# for i in range(a,b):
+#     if i%2==0:
+#         print(i,end=" ")
+
+#2
+# a = int(input("enter your num --> "))
+# b = int(input("enter your num --> "))
+# c = int(input("enter your num --> "))
+# d = int(input("enter your num --> "))
+# for i in range(a,b):
+#     if i%d==c:
+#         print(i)
+
+#3
+# a=int(input("enter your num --> "))
+# b=int(input("enter your num --> "))
+# cnt=a
+# while (cnt*cnt)<=b:
+#     print(cnt*cnt)
+#     cnt+=1
+
+#4
+
+#5
+# n = int(input("enter your number --> "))
+# sum = 0
+# for i in range(n,1,-1):
+#     m=n%10
+#     sum+=m
+#     n=n//10
+# print(sum)
+
+#6
+# n = int(input("enter your number --> "))
+# rev = 0
+# for i in range(n,1,-1):
+#     if n==0:
+#         print("",end="")
+#     else:
+#         m=n%10
+#         rev=rev*10+m
+#         n=n//10
+# print(rev)
+
+#7
+# x = int(input("enter your number --> "))
+# for i in range(2,x+1):
+#     if x%i==0:
+#         print(i)
+#         break
+
+#8
+# x = int(input("enter your num --> "))
+# for i in range(1,x+1):
+#     if x%i==0:
+#         print(i, end=' ')
+
+#9
+# x = int(input("enter your num --> "))
+# cnt=0
+# for i in range(1,x+1):
+#     if x%i==0:
+#         cnt+=1
+# print(cnt)
+
+#10
+# cnt=0
+# for i in range(1,101):
+#     cnt+=i
+# print(cnt)
+
+
+#11
+# n = int(input("enter the amount of numbers you vant to input --> "))
+# s = 0
+# for i in range(n):
+#     num = int(input())
+#     s += num
+# print(s)
+
+#12
+
+#13
+# n = int(input("enter the amount of numbers you vant to input --> "))
+# s = 0
+# for i in range(n):
+#     num = int(input())
+#     if num==0:
+#         s += 1
+# print(s)
+
+#14
+# n = int(input("enter the amount of numbers you vant to input --> "))
+# zeros = 0
+# possitive = 0
+# negative = 0
+# for i in range(n):
+#     num = int(input())
+#     if num==0:
+#         zeros += 1
+#     elif num>0:
+#         possitive += 1
+#     elif num<0:
+#         negative += 1
+# print(zeros,possitive,negative)
+
+#15
+# n = int(input("enter the amount of numbers you vant to input --> "))
+# s = 0
+# for i in range(n):
+#     num = int(input())
+#     if num==0:
+#         s += 1
+# if s>0:
+#     print("YES")
+# elif s==0:
+#     print("NO")
+
+#16
+#17
+#18
+#19
+
+# ---- while
+
+#1
+# a=int(input("enter your num --> "))
+# cnt=1
+# while cnt*cnt<=a:
+#     print(cnt*cnt)
+#     cnt+=1
+
+#2
+# a = int(input("enter your number --> "))
+# b=2
+# while b<=a:
+#     if a%b==0:
+#         print(b)
+#         break
+#     b+=1
+
+#3
+#4
+#5
+#6
+#7
+#8
+#9
+#10
+#11
